de360fd86adb183793c8a235854726f2055faef2
Matthew McCall <mmccall@jhsph.edu>
2011-03-10 22:32:28 +0000

    removed obsolete functions; fixed typo in documentation
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/frmaTools@53584 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makeVectorPackage.R
+++ R/makeVectorPackage.R
@@ -1,22 +1,14 @@
-makeVectorPackage <- function(files, batch.id, version, maintainer, species, type="AffyBatch", target=NULL, file.dir=".", output.dir=".", unlink=TRUE, verbose=TRUE){
+makeVectorPackage <- function(files, batch.id, version, maintainer, species, annotation, packageName, background="rma", normalize="quantile", normVec=NULL, type="AffyBatch", target=NULL, file.dir=".", output.dir=".", unlink=TRUE, verbose=TRUE){
 
   type <- match.arg(type, c("AffyBatch", "ExonFeatureSet"))
 
+  if(type=="AffyBatch") platform <- gsub("cdf","",annotation)  
   if(type=="ExonFeatureSet"){
+    platform <- annotation
     target <- match.arg(target, c("core", "full", "extended", "probeset"))
     require(oligo)
   }
-  
-  if(type=="AffyBatch"){
-    cdfname <- cleancdfname(cdfName(object))
-    platform <- gsub("cdf","",cdfname)
-    pkgname <- paste(platform, "frmavecs", sep="")
-  }
-  if(type=="ExonFeatureSet"){
-    cdfname <- annotation(object)
-    platform <- paste(cdfname, target, sep="")
-    pkgname <- paste(platform, "frmavecs", sep="")
-  }
+
   thispkg <- "frmaTools"
   desc <- packageDescription(thispkg)
   thispkgVers <- desc$Version
@@ -29,16 +21,16 @@ makeVectorPackage <- function(files, batch.id, version, maintainer, species, typ
                        SPECIES = species)
 
   
-  createdPkg <- createPackage(pkgname,
+  createdPkg <- createPackage(packageName,
                               destinationDir=output.dir,
                               originDir=system.file("VectorPkg-template", package="frmaTools"),
                               symbolValues=symbolValues,
                               unlink=unlink)
 
-  if(type=="AffyBatch") vecs <- makeVectorsAffyBatch(files, batch.id, file.dir, verbose)
-  if(type=="ExonFeatureSet") vecs <- makeVectorsExonFeatureSet(files, batch.id, target, file.dir, verbose)
-  assign(pkgname, vecs)
-  save(list=eval(pkgname), file=file.path(createdPkg$pkgdir, "data", paste(pkgname, ".rda", sep="")), compress=TRUE)
+  if(type=="AffyBatch") vecs <- makeVectorsAffyBatch(files, batch.id, background, normalize, normVec, annotation, file.dir, verbose)
+  if(type=="ExonFeatureSet") vecs <- makeVectorsExonFeatureSet(files, batch.id, target, annotation, background, normalize, normVec, file.dir, verbose)
+  assign(packageName, vecs)
+  save(list=eval(packageName), file=file.path(createdPkg$pkgdir, "data", paste(packageName, ".rda", sep="")), compress=TRUE)
 }
 
 
